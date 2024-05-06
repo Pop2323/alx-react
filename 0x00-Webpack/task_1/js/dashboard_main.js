bd73e4d104c34433c6aad3cab6ca0709cc9c8f29
Pop2323 <omar4500455@gmail.com>
2024-05-06 15:28:36 -0700

    webpack

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,19 +1,17 @@
-const $ = require('jquery');
-const _ = require('lodash');
+import $ from 'jquery';
+import _ from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append("<p id='count'></p>");
+$('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-const updateCounter = () => {
-	let count = $('#count').html() || 0;
-	$('button').on('click', () => {
-		count++;
-		$('#count').html(`${count} clicks on the button`);
-	});
+let count = 0;
+
+function updateCounter() {
+  count++;
+  $("#count").html(`${count} clicks on the button`);
 };
 
-_.debounce(updateCounter, 500);
-updateCounter();
+$('button').on('click', _.debounce(updateCounter, 500));
